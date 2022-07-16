2ded09faa88ce6e11d6002c1f548bae14aa4044a
kimhayoung <85178486+carrothay@users.noreply.github.com>
2022-07-16 21:32:38 +0900

    16 July
    BOJ Lv3 반복문 문제6 기찍 N

---
+++ 2022July/BJ_2742.java
@@ -0,0 +1,15 @@
+package bj_003;
+
+import java.util.Scanner;
+
+public class BJ_2742 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = n; i > 0; i--) {
+			System.out.println(i);
+		}
+		sc.close();
+	}
+}
