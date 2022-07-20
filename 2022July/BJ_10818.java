1b407efb52dbf8eab0a3a4acac864bbf00a921fe
kimhayoung <85178486+carrothay@users.noreply.github.com>
2022-07-20 16:52:33 +0900

    20 July: Array
    BOJ Lv4 1차원 배열 문제1 최소, 최대 - 첫번째 답

---
+++ 2022July/BJ_10818.java
@@ -0,0 +1,30 @@
+package bj_004;
+
+import java.util.Scanner;
+
+public class BJ_10818 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		int max = arr[0];
+		int min = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+
+		System.out.println(min + " " + max);
+	}
+}
